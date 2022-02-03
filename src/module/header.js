ef46a0c67081000fe356b33b45b4d9f4681841c0
Utsav34 <utsav.goel@successive.tech>
2022-02-03 20:01:18 +0530

    reacttask

--- src/module/header.js
+++ src/module/header.js
@@ -1,27 +1,44 @@
-import React from 'react'
-import { useContext } from "react"
-import { Link } from 'react-router-dom'
-import { Ul,Li } from "../components/styles";
-import ThemeContext from "../page/themecontext"
-// import {useNavigate} from "react-router";
-// import { ThemeContext } from "@mui/styled-engine";
+import * as React from 'react';
+import {Nav, NavLink, NavMenu, NavBtn,NavBtnLink } from '../components/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+const Header = () => {
+      const removeSession=()=>{
+      localStorage.removeItem("getToken")
+  }
+  // const themes = useContext(ThemeContext);
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+        <Nav>
+                <NavMenu>
+                     <NavLink to='/Dashboard' activeStyle>
+                       Dashboard
+                      </NavLink>
+                      <NavLink to='/Contact' activeStyle>
+                        Contact
+                      </NavLink>
+                     <NavLink to='/aboutUs' activeStyle>
+                       About Us
+                     </NavLink>
+                     <NavLink to='/Modal' activeStyle>
+                       Modal
+                     </NavLink>
+                   </NavMenu>
+                    <NavBtn>
+                      <NavBtnLink to="/" onClick={removeSession} >Logout</NavBtnLink>
+                    </NavBtn>
+                  </Nav>
 
-const Header = () =>{    
-const removeSession=()=>{
-    localStorage.removeItem("getToken")
-}
-const themes = useContext(ThemeContext);
-    return(
-        <div style={themes.theme}>
-            <Ul>   
-                <Li><Link to="/Dashboard">Dashboard</Link></Li>
-                <Li><Link to="/Contact" >Contact</Link></Li>
-                <Li><Link to="/aboutUs">About us</Link></Li>
-                <Li><Link to="/modal">Modal</Link></Li>
-                <Li><Link to="/" onClick={removeSession} >Logout</Link></Li>
-            </Ul>
-         {/* </ThemeContext.Provider> */}
-        </div>
-    )
-}
+        
+     
+
+         
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 export default Header;
